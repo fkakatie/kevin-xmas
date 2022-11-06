c508837f4a08510c316c60e6ab85dbded11bbfff
Katie Sexton <ksexton@adobe.com>
2022-11-06 16:26:34 -0700

    update: 2020 bday

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,56 @@
-const getCode = () => {
-    const $code = document.getElementById('code')
-    const code = $code.value.toLowerCase().trim();
-    if (code === '') {
-        $code.classList.add('error');
+const win = () => {
+  const gradeBtn = document.getElementById('grade');
+  gradeBtn.setAttribute('aria-hidden', true);
+  const success = document.querySelector('.success');
+  success.setAttribute('aria-hidden', false);
+}
+
+const gradeQuiz = () => {
+  const questions = document.querySelectorAll('.question');
+  const answers = ['calcium', 'icosahedron', 'score'];
+  let correct = 0;
+  questions.forEach((q) => {
+    const options = q.querySelectorAll('input[type="radio"]');
+    const answer = [...options].find((radio) => radio.checked);
+    if (answer && answer.value && answers.includes(answer.value)) { // correct
+      q.classList.remove('wrong');
+      answer.closest('div').classList.add('correct');
+      options.forEach((option) => {
+        if (!option.checked) option.disabled = true;
+      });
+      correct++;
+    } else { // incorect
+      q.classList.add('wrong');
+      answer.disabled = true;
+      answer.closest('label').classList.add('disabled');
     }
-    return code;
+  });
+  if (correct === answers.length) win();
 }
 
 const getEmail = () => {
-    const $email = document.getElementById('email');
-    const email = $email.value.trim();
-    if (email === '') {
-        $email.classList.add('error');
-    }
-    return email;
+  const emailEl = document.getElementById('email');
+  const email = emailEl.value.trim();
+  if (email === '') emailEl.classList.add('error');
+  return email;
 }
 
 const checkSubmission = (e) => {
-    e.preventDefault();
-    const code = getCode();
-    const email = getEmail();
-    if (code !== '' && email !== '') {
-        submitForm(code, email);
-    }
+  e.preventDefault();
+  const email = getEmail();
+  if (email !== '') submitForm(email);
 }
 
-const submitForm = async (code, email) => {
-    const url = `https://script.google.com/macros/s/AKfycbwHHhF6zRXz1zNJgiPZWmEe8vZA-aCVh5YnZviQGMHYcp0X3GKPPwvC2x5BBl6L13I/exec?code=${code}&email=${email}`;
-    const req = await fetch(url);
-    const res = await req.json();
-    alert(res.response);
+const submitForm = async(email) => {
+  const url = `https://script.google.com/macros/s/AKfycbybYaCQCwDbku8XJSCiJVWz5Lua1ny2aCD4uIvEexMOelvnaqtyUfNj7MH6eC_38MBv/exec?email=${email}`;
+  const req = await fetch(url);
+  const res = await req.json();
+  alert(res.response);
 }
 
 window.addEventListener('load', (e) => {
-    const $submitBtn = document.getElementById('submit');
-    $submitBtn.addEventListener('click', checkSubmission);
+  const gradeBtn = document.getElementById('grade');
+  gradeBtn.addEventListener('click', gradeQuiz);
+  const submitBtn = document.getElementById('submit');
+  submitBtn.addEventListener('click', checkSubmission);
 });
